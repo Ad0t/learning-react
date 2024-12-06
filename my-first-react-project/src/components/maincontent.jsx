88a237feac62564fdd76aca2a39ac5642243c259
Ad0t <171239099+Ad0t@users.noreply.github.com>
2024-12-06 14:17:01 +0530

    some-progress

---
+++ my-first-react-project/src/components/maincontent.jsx
@@ -0,0 +1,25 @@
+export default function MainContent () {
+    return (
+        <div className="content">
+            <h1>G-Wagon</h1>
+            <p>The Mercedes-Benz G-Class, commonly known as the G-Wagon (short for Geländewagen, meaning "terrain vehicle" in German), is a luxury SUV renowned for its off-road capabilities, iconic boxy design, and premium features. Originally developed as a military vehicle in the 1970s, the G-Wagon has evolved into a symbol of luxury and performance, blending ruggedness with opulence.</p>
+            <h1>Key Features</h1>
+            <ol>
+                <li>Design:
+                    <ul>
+                        <li>Distinctive boxy silhouette with sharp lines and flat surfaces.</li>
+                        <li>Iconic circular LED headlights and a large, bold grille.</li>
+                        <li>Premium materials and advanced technology in the interior.</li>
+                    </ul>
+                </li>
+                <li>Off-Road Capability:
+                    <ul>
+                        <li>Three fully locking differentials (front, center, and rear).</li>
+                        <li>Robust ladder-frame chassis for durability.</li>
+                        <li>High ground clearance and advanced 4MATIC all-wheel-drive system.</li>
+                    </ul>
+                </li>
+            </ol> 
+        </div>
+    )
+} 
--- my-first-react-project/src/main.jsx
+++ my-first-react-project/src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import NavBar from './components/nav'
-import MainContent from './components/main'
+import MainContent from './components/maincontent'
 
 createRoot(document.getElementById('root')).render(
   <>
